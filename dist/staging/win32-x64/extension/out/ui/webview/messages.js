cae8d0b71cae910dbe542ad22276d0bdcdb5a838
josuemoraishg <josue@ufu.br>
2026-07-07 13:24:02 -0300

    update UI

--- dist/staging/win32-x64/extension/out/ui/webview/messages.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WEBVIEW_MESSAGE_VERSION = void 0;
-exports.isHostMessage = isHostMessage;
-exports.WEBVIEW_MESSAGE_VERSION = 1;
-function isHostMessage(value) {
-    return typeof value === "object" && value !== null && "type" in value && "version" in value;
-}
-//# sourceMappingURL=messages.js.map
